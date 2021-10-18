98cd1a590b651e05aa83dc912a69df63f4c34163
imgwang-yeol <mkitigy@gmail.com>
2021-10-19 00:18:56 +0900

    chore: set path alias of components, constants

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,16 @@
 const path = require('path');
 
+exports.onCreateWebpackConfig = ({ actions }) => {
+  actions.setWebpackConfig({
+    resolve: {
+      alias: {
+        '@components': path.resolve(__dirname, 'src/components'),
+        '@constants': path.resolve(__dirname, 'src/constants'),
+      },
+    },
+  });
+};
+
 exports.createPages = async ({ graphql, actions }) => {
   const query = await graphql(`
     query Posts {
@@ -25,7 +36,7 @@ exports.createPages = async ({ graphql, actions }) => {
   const posts = query.data.allMdx.edges;
   const postTemplate = path.resolve(__dirname, 'src/pages', 'template.tsx');
 
-  posts.forEach(post => {
+  posts.forEach((post) => {
     const { frontmatter, body } = post.node;
     const path = `/post/${frontmatter.title.trim().replace(/\s+/g, '-')}`;
 
